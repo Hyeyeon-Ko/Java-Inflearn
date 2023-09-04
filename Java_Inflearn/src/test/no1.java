8d41441960d5f2a4d860ab7bfe380cd0e0de46df
Hyeyeon-Ko <khy33355@gmail.com>
2023-09-04 13:39:48 +0900

    Section1, 2 Complete!

---
+++ Java_Inflearn/src/test/no1.java
@@ -0,0 +1,20 @@
+package test;
+
+public class no1 {
+
+	public static void main(String[] args) {
+
+		char grade;
+		int score = 80;
+		
+		if (score >= 90) {
+			grade = 'A';
+		}
+		else {
+			grade = 'B';
+		}
+		
+		System.out.println(grade);
+	}
+
+}
---
+++ Java_Inflearn/src/test/no3.java
@@ -0,0 +1,29 @@
+package test;
+
+public class no3 {
+
+	public static void main(String[] args) {
+
+		int dan;
+		int times;
+		
+		//1번쨰 방법: for문 안에 조건문 걸어주기 
+		for (dan = 1; dan <= 9; dan++) {
+			for (times = 1; times <= 9; times++) {
+				if (dan < times) break;
+				
+				System.out.println(dan + " X " + times + " = " + dan*times);
+			}
+			System.out.println();
+		}
+		
+		//2번째 방법: for문 자체에 조건 걸어주기 
+		for (dan = 1; dan <= 9; dan++) {
+			for (times = 1; times <= dan; times++) {
+				System.out.println(dan + " X " + times + " = " + dan*times);
+			}
+			System.out.println();
+		}
+	}
+
+}

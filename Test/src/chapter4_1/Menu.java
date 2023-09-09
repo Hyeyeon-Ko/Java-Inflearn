7c4859b0beac812902fa13cbba21ae708c1e5fd7
Hyeyeon-Ko <khy33355@gmail.com>
2023-09-09 22:41:17 +0900

    chapter2, 4 test 완

---
+++ Test/src/chapter4_1/Menu.java
@@ -0,0 +1,10 @@
+package chapter4_1;
+
+public class Menu {
+
+	public static final int STARMERICANO = 4000;
+	public static final int STARLATTE = 4300;
+	
+	public static final int BEANAMERICANO = 4000;
+	public static final int BEANLATTE = 4500;
+}
---
+++ Test/src/chapter4_1/StarCoffee.java
@@ -0,0 +1,18 @@
+package chapter4_1;
+
+public class StarCoffee {
+
+	int money;
+	
+	public String bewing(int money) {
+		this.money += money;
+		if(money == Menu.STARMERICANO) {
+			return "별다방 아메리카노를 구입했습니다.";
+		}
+		else if(money == Menu.STARLATTE) { 
+			return "별다방 라떼를 구입했습니다.";
+		}
+		else 
+			return null;
+	}
+}
